58a6ad54a8228f6969d7f6ae8560734272010f1b
JesusLoveDK <67304802+JesusLoveDK@users.noreply.github.com>
2023-05-15 20:40:03 +0900

    .js test

---
+++ nodeJS/server_basic/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+let server = http.createServer(function(request,response){
+    
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.end('Hello Node.js!!');
+
+});
+
+server.listen(8080, function(){
+    console.log('Server is Running');
+})
