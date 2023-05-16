6620101f675017c54bc7b33ab5694507e10ddf82
JesusLoveDK <67304802+JesusLoveDK@users.noreply.github.com>
2023-05-16 11:10:39 +0900

    commit test again

--- dream_coding/test.js
+++ dream_coding/test.js
@@ -3,9 +3,13 @@ window.onload = function(){
         confirm(question) ? yes() : no();
     }
     
-    ask(
+    // 함수표현식 테스트
+    // 자바스크립트는 함수를 "특별한 종류의 값으로 취급" => "특별한 동작을 하는 '구조'" 로 취급하는 다른 언어와는 차이가 있다
+    let ask_test = ask;
+
+    ask_test(
         "동의하십니까?",
-        () => alert('동의하셨습니다'),
+        () => alert('동의하셨습니다.'),
         () => alert('취소 버튼을 누르셨습니다.'),
     )
 }
