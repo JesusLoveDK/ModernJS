18e6eecff96746b4fb0c1962da8b8fd112396033
JesusLoveDK <67304802+JesusLoveDK@users.noreply.github.com>
2023-05-16 13:28:54 +0900

    commit

--- dream_coding/test.js
+++ dream_coding/test.js
@@ -1,15 +1,22 @@
 window.onload = function(){
-    function ask(question, yes, no) {
-        confirm(question) ? yes() : no();
-    }
     
-    // 함수표현식 테스트
-    // 자바스크립트는 함수를 "특별한 종류의 값으로 취급" => "특별한 동작을 하는 '구조'" 로 취급하는 다른 언어와는 차이가 있다
-    let ask_test = ask;
-
-    ask_test(
-        "동의하십니까?",
-        () => alert('동의하셨습니다.'),
-        () => alert('취소 버튼을 누르셨습니다.'),
-    )
 }
+
+// TODO: 테스트할것
+function ask(question, yes, no) {
+    confirm(question) ? yes() : no();
+}
+
+// 함수표현식 테스트
+// 자바스크립트는 함수를 "특별한 종류의 값으로 취급" => "특별한 동작을 하는 '구조'" 로 취급하는 다른 언어와는 차이가 있다
+let ask_test = ask;
+
+// 화살표 함수 테스트
+// ask_test(
+//     "동의하십니까?",
+//     () => alert('동의하셨습니다.'),
+//     () => alert('취소 버튼을 누르셨습니다.'),
+// )
+
+console.log('test');
+console.log('test');
