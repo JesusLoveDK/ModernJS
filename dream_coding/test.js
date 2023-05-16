14d1bd22e8f33ab53b6bf65b2ae4d6184d1da0ad
JesusLoveDK <67304802+JesusLoveDK@users.noreply.github.com>
2023-05-16 10:41:27 +0900

    message

---
+++ dream_coding/test.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    function ask(question, yes, no) {
+        confirm(question) ? yes() : no();
+    }
+    
+    ask(
+        "동의하십니까?",
+        () => alert('동의하셨습니다'),
+        () => alert('취소 버튼을 누르셨습니다.'),
+    )
+}
